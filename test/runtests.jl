183413b1e63c1057c6e9c97723dd35cd2ad95087
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2023-11-17 11:32:33 -0700

    tests: Support other GPU vendors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,16 @@
 using PSOGPU
 using Test, StaticArrays, LinearAlgebra, Random
 
-const GROUP = get(ENV, "GROUP", "All")
+const GROUP = get(ENV, "GROUP", "CPU")
 
 @testset "Rosenbrock test dimension = $(n)" for n in 2:4
     global N = n
     include("./regression.jl")
 end
 
-if GROUP == "CUDA"
+if GROUP != "CPU"
+    @eval using $(Symbol(GROUP))
+
     @testset "Rosenbrock on gpu" begin
         include("./gpu.jl")
     end
