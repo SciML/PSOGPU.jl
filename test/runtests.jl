00966cf781243605edb265f7eb884a7a52994d8a
Utkarsh <rajpututkarsh530@gmail.com>
2025-03-26 01:16:32 -0400

    format

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ global CI_GROUP = get(ENV, "GROUP", "CPU")
 @safetestset "Reinitialization tests" include("./reinit.jl")
 
 #TODO: Curent throws warning for redefinition with the use of @testset multiple times. Migrate to TestItemRunners.jl
-@testset for  BACKEND in unique(("CPU", CI_GROUP))
+@testset for BACKEND in unique(("CPU", CI_GROUP))
     global GROUP = BACKEND
     @testset "$(BACKEND) optimizers tests" include("./gpu.jl")
     @testset "$(BACKEND) optimizers with constraints tests" include("./constraints.jl")
